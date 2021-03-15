6424900576062ffa289c58928917da7c4b62baea
axelvonkamp <axelk1@gmx.de>
2021-03-15 14:08:51 +0100

    restructured into package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='optlang_enumerator',
+    version='0.0.1',
+    description='Enumeration of multiple solutions to a MILP with optlang.',
+    url='https://github.com/cnapy-org/optlang_enumerator.git',
+    author='Axel von Kamp',
+    author_email='axelk1@gmx.de',
+    license='Apache License 2.0',
+    packages=['optlang_enumerator'],
+    zip_safe=False)
