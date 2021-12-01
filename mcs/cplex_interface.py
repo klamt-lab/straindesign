4710592569b2581b95fae6a016383bd97daaa874
VonAlphaBisZulu <schneiderp@mpi-magdeburg.mpg.de>
2021-12-01 14:58:43 +0100

    Update cplex_interface.py

--- mcs/cplex_interface.py
+++ mcs/cplex_interface.py
@@ -4,6 +4,22 @@ import cplex as cp
 import cobra
 from cplex.exceptions import CplexError
 
+# Collection of CPLEX-related functions that facilitate the creation
+# of CPLEX-object and the solutions of LPs/MILPs with CPLEX from
+# vector-matrix-based problem setups.
+#
+# functions: 
+# 1. init_cpx_milp: Creates a CPLEX-object from a matrix-based problem setup.
+#                   Supports LPs, MILPs (including indicator constraints)
+#
+# 2. cplex_fba: Perform FBA for cobra model with CPLEX
+# 3. cplex_fva: Perform FVA for cobra model with CPLEX
+#
+#   Philipp Schneider (schneiderp@mpi-magdeburg.mpg.de)
+# - December 2021
+#  
+
+# Create a CPLEX-object from a matrix-based problem setup
 def init_cpx_milp(c,A_ineq,b_ineq,A_eq,b_eq,lb,ub,vtype=None,ic_binv=None,ic_A_ineq=None,ic_b_ineq=None,ic_sense=None,ic_indicval=None,x0=None,options=None):
     
     prob = cp.Cplex()
@@ -54,7 +70,9 @@ def init_cpx_milp(c,A_ineq,b_ineq,A_eq,b_eq,lb,ub,vtype=None,ic_binv=None,ic_A_i
     prob.set_results_stream(None)
     return prob
 
+# FBA for cobra model with CPLEX
 def cplex_fba(model):
+    # prepare vectors and matrices
     S = cobra.util.create_stoichiometric_matrix(model)
     S = sparse.csr_matrix(S)
     lb = [v.lower_bound for v in model.reactions]
@@ -63,6 +81,7 @@ def cplex_fba(model):
     numreac = len(model.reactions)
     if model.objective_direction == 'max':
         c = [ -i for i in c]
+    # build CPLEX object
     cpx = init_cpx_milp(c,[],[],S,[0]*numreac,lb,ub)
     try:
         cpx.solve()
@@ -77,8 +96,9 @@ def cplex_fba(model):
         fv = [np.nan] * numreac
         return fv, max_v
 
+# FVA for cobra model with CPLEX
 def cplex_fva(model,reacs=None):
-    # prepare FVA CPLEX
+    # prepare vectors and matrices
     S = cobra.util.create_stoichiometric_matrix(model)
     S = sparse.csr_matrix(S)
     lb = [v.lower_bound for v in model.reactions]
