2a0805e4601545580f31f7b62617e0ab2a6879a0
Philipp Schneider <schneiderp@mpi-magdeburg.mpg.de>
2022-04-26 18:17:20 +0200

    fixed some stuff with spawning pool workers

--- examples/03_benchmark_ECC2_23BDO.py
+++ examples/03_benchmark_ECC2_23BDO.py
@@ -1,25 +1,23 @@
 import cobra
 from straindesign.names import *
 import numpy as np
-from importlib import reload
-import mcs
 import os
+import straindesign
 from scipy import sparse, io
 #import traceback
 #import warnings
-import sys
 
 # load network
 network = cobra.io.read_sbml_model(os.path.dirname(os.path.abspath(__file__))+"/ECC2_23BDO.sbml")
 
 # specify modules
-modules  = [straindesign.SD_Module(network,"mcs_lin",module_sense="desired",constraints=["BIOMASS_Ec_iJO1366_core_53p95M >= 0.05"])]
-modules += [straindesign.SD_Module(network,"mcs_lin",module_sense="undesired",constraints=["EX_23bdo_e + 0.3 EX_glc__D_e <= 0"])]
+modules  = [straindesign.SDModule(network,"mcs_lin",module_sense="desired",constraints=["BIOMASS_Ec_iJO1366_core_53p95M >= 0.05"])]
+modules += [straindesign.SDModule(network,"mcs_lin",module_sense="undesired",constraints=["EX_23bdo_e + 0.3 EX_glc__D_e <= 0"])]
 
 # specify MCS setup
 maxSolutions = np.inf
 maxCost = 9
-solver = 'cplex'
+solver = 'glpk'
 
 ko_cost = {'EX_o2_e'	: 1.0}
 # ki_cost = None
@@ -31,9 +29,9 @@ mcsEnum = straindesign.StrainDesigner(network,modules,compress=True, gko_cost = 
 # mcsEnum = straindesign.StrainDesignMILP(network,modules, max_cost=maxCost, solver=solver,M=M)
 
 # solve MILP
-solutions = mcsEnum.enumerate(max_solutions=maxSolutions)
+# solutions = mcsEnum.enumerate(max_solutions=maxSolutions)
 # solutions = mcsEnum.compute_optimal(max_solutions=maxSolutions)
-# solutions = mcsEnum.compute(max_solutions=3)
+solutions = mcsEnum.compute(max_solutions=3)
 print(len(solutions.get_strain_designs()))
 io.savemat('mcs_python.mat', mdict={'whatever_data': \
     [','.join(a) for a in [[k for k,v in m.items()] for m in solutions.get_strain_designs()]]})
