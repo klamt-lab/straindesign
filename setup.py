32e22c9109c9b478f3088f3aa55b18cb3a956f14
Philipp Schneider <schneiderp@mpi-magdeburg.mpg.de>
2022-04-26 11:25:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from setuptools import setup,find_packages
 
 setup(name='straindesign',
-    version='0.32',
+    version='0.1',
+    description='Computation of Strain Designs.',
     url='https://github.com/klamt-lab/straindesign.git',
     description="Computational strain design package for the COBRApy framework",
-    long_description="Computational strain design package for the COBRApy framework, offering standard and advanced tools for the analysis and redesign of biological networks",
-    long_description_content_type="text/plain",
     author='Philipp Schneider',
     author_email='zgddtgt@gmail.com',
     license='Apache License 2.0',
     python_requires='>=3.7',
-    package_data={'straindesign': ['efmtool.jar']},
     packages=find_packages(),
-    install_requires=['cobra','jpype1','scipy','matplotlib'],
+    install_requires=['numpy', 'scipy', 'sympy', 'cobra', 'optlang', 'efmtool_link', 'swiglpk','pickle','json','re'],
     zip_safe=False)
