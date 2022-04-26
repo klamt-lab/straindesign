955a41f0a9a821a4f072036984cf0f49c31aad31
Philipp Schneider <schneiderp@mpi-magdeburg.mpg.de>
2022-04-26 14:30:59 +0200

    Revert "remove examples and matlab files"
    This reverts commit 9d14832476de9973d90a2f7d5cf024165ca6e353.

---
+++ examples/01_mcs_test_iML1515core.py
@@ -0,0 +1,79 @@
+import cobra
+import numpy as np
+from importlib import reload
+import straindesign
+import os
+from scipy import sparse
+from straindesign.names import *
+#import traceback
+#import warnings
+import sys
+
+# load network
+network = cobra.io.read_sbml_model(os.path.dirname(os.path.abspath(__file__))+"/iML1515core.sbml")
+# cobra.flux_analysis.flux_variability_analysis(network)
+# network = cobra.io.read_sbml_model("iML1515core.sbml")
+# results = straindesign.fva(network,solver='cplex')
+# remove external metabolites and obsolety
+# e reactions
+external_mets = [i for i,cpts in zip(network.metabolites,network.metabolites.list_attr("compartment")) if cpts == 'External_Species']
+network.remove_metabolites(external_mets)
+S = cobra.util.create_stoichiometric_matrix(network)
+obsolete_reacs = [reac for reac,b_rempty in zip(network.reactions,np.any(S,0)) if not b_rempty]
+network.remove_reactions(obsolete_reacs)
+# close some uptakes
+# network.reactions.AcUp.upper_bound = 0
+# network.reactions.GlycUp.upper_bound = 0
+
+sol = straindesign.fba(network,constraints=["EX_o2_e=0"])
+# specify modules
+modules = straindesign.SDModule(network,"mcs_bilvl",module_sense="desired",constraints=["2 BIOMASS_Ec_iML1515_core_75p37M >= 0.1","EX_etoh_e >= 1"],inner_objective="BIOMASS_Ec_iML1515_core_75p37M")
+
+network.metabolites.list_attr("compartment")
+# specify MCS setup
+maxSolutions = np.inf
+maxCost = 7
+solver = 'cplex'
+ko_cost = { 'PTAr' 		: 1,
+            'ACKr'		: 2,
+            'FORtppi'	: 1.4,	
+            'SUCDi'	    : 1.2,
+            'H2Otpp'	: 2.6,	
+            'ATPS4rpp'	: 1.3,
+            'PFL'		: 0.7,
+            'GLYCtex'	: 0.9,	
+            'EX_ac_e'	: 0.4,	
+            'ACtex'	    : 1.2,
+            'CYTBO3_4pp': 2.1,	
+            'O2tpp'		: 0.24,
+            'O2tex'		: 0.77,
+            'EX_o2_e'	: 0.4,	
+            'POR5'		: 1.5,
+            'CO2tpp'	: 3,	
+            'ALCD19'	: 2,	
+            'ASPtpp' 	: 0.25}
+ki_cost = { 'EDD' 	 	: 0.5,
+            'GAPP' 	 	: 0.4,
+            'ENO'	 	: 0.6,
+            'EDA' 	 	: 1.3,
+            'AKGDH'  	: 0.7,
+            'MALD' 	 	: 1.8,
+            'NADTRHD'  	: 0.12,
+            'LCARS'	 	: 0.13,
+            'SUCCt1pp'	: 0.2,
+            'SUCFUMtpp'	: 0.1}
+# ko_cost = {'EX_o2_e'	: 0.4}
+# ki_cost = None
+# gko_cost = None
+
+M=None
+# construct MCS MILP
+# mcsEnum = straindesign.StrainDesigner(network,modules, max_cost=maxCost,ko_cost=ko_cost, ki_cost=ki_cost, gko_cost=gko_cost, solver=solver,M=M)
+mcsEnum = straindesign.StrainDesigner(network,modules, max_cost=maxCost,ko_cost=ko_cost, ki_cost=ki_cost, solver=solver,M=M)
+# mcsEnum = straindesign.StrainDesignMILP(network,modules,ko_cost=ko_cost, ki_cost=ki_cost, max_cost=maxCost,solver=solver,M=None)
+
+# solve MILP
+mcsEnum.enumerate(max_solutions=maxSolutions)
+# mcsEnum.compute_optimal(max_solutions=maxSolutions)
+# rmcs = mcsEnum.compute(max_solutions=maxSolutions,time_limit=15)
+pass
---
+++ examples/04_ECC2comp_example.py
@@ -0,0 +1,87 @@
+#%%
+import cobra
+import optlang
+import straindesign.mcs_computation as mcs_computation
+import numpy
+import scipy
+
+#%% 
+from importlib import reload
+import straindesign
+reload(straindesign)
+import straindesign.mcs_computation as mcs_computation
+
+#%%
+ecc2 = cobra.io.read_sbml_model("ECC2comp.sbml")
+# allow all reactions that are not boundary reactions as cuts (same as exclude_boundary_reactions_as_cuts option of compute_mcs)
+cuts = numpy.array([not r.boundary for r in ecc2.reactions])
+reac_id = ecc2.reactions.list_attr('id') # list of reaction IDs in the model
+# define target (multiple targets are possible; each target can have multiple linear inequality constraints)
+ecc2_mue_target = [[("Growth", ">=", 0.01)]] # one target with one constraint, a.k.a. syntehtic lethals
+# this constraint alone would not be sufficient, but there are uptake limits defined in the reaction bounds
+# of the model that are integerated automatically by the compute_mcs function into all target and desired regions
+# convert into matrix/vector relation format
+ecc2_mue_target = [mcs_computation.relations2leq_matrix(
+                   mcs_computation.parse_relations(t, reac_id_symbols=mcs_computation.get_reac_id_symbols(reac_id)), reac_id)
+                   for t in ecc2_mue_target]
+# convert non-default bounds of the newtork model into matrix/vector relation format
+# in this network these are substrate uptake bounds
+# bounds_mat, bounds_rhs = mcs_computation.reaction_bounds_to_leq_matrix(ecc2)
+# integrate the relations defined through the network bounds into every target (still matrix/vector relation format)
+# ecc2_mue_target = [(scipy.sparse.vstack((t[0], bounds_mat), format='csr'), numpy.hstack((t[1], bounds_rhs))) for t in ecc2_mue_target]
+mcs_computation.integrate_model_bounds(ecc2, ecc2_mue_target)
+# convert into constraints that can be added to the COBRApy model (e.g. in context)
+ecc2_mue_target_constraints= mcs_computation.get_leq_constraints(ecc2, ecc2_mue_target)
+for c in ecc2_mue_target_constraints[0]: # print constraints that make up the first target
+    print(c)
+#%%
+ecc2_mcs,_ = mcs_computation.compute_mcs(ecc2, ecc2_mue_target, cuts=cuts, enum_method=3, max_mcs_size=3, network_compression=True, include_model_bounds=False)
+print(len(ecc2_mcs))
+# show MCS as n-tuples of reaction IDs
+ecc2_mcs_rxns= [tuple(reac_id[r] for r in mcs) for mcs in ecc2_mcs]
+print(ecc2_mcs_rxns)
+# check that all MCS disable the target
+print(all(mcs_computation.check_mcs(ecc2, ecc2_mue_target[0], ecc2_mcs, optlang.interface.INFEASIBLE)))
+
+# %% same calculation without network compression
+ecc2_mcsF,_ = mcs_computation.compute_mcs(ecc2, ecc2_mue_target, cuts=cuts, enum_method=3, max_mcs_size=3, network_compression=False, include_model_bounds=False)
+print(set(ecc2_mcs) == set(ecc2_mcsF))
+
+# %%
+import pickle
+with open("ecc2_straindesign.pkl","rb") as f:
+    ref = pickle.load(f)
+set(ecc2_mcs) - ref
+
+# %% full FVA
+# with ecc2 as model:
+model = ecc2.copy() # copy model because switching solver in context sometimes gives an error (?!?)
+model.objective = model.problem.Objective(0)
+fva_tol = 1e-8 # with CPLEX 1e-8 leads to removal of EX_adp_c, 1e-9 keeps EX_adp_c
+model.tolerance = fva_tol # prevent essential EX_meoh_ex from being blocked, sets solver feasibility/optimality tolerances
+# model.solver.configuration.tolerances.feasibility = 1e-9
+model.solver = 'glpk_exact' # appears to make problems for context management
+# model_mue_target_constraints= get_leq_constraints(model, ecc2_mue_target)
+# model.add_cons_vars(model_mue_target_constraints[0])
+fva_res = cobra.flux_analysis.flux_variability_analysis(model, fraction_of_optimum=0, processes=1) # no interactive multiprocessing on Windows
+print(fva_res.loc['EX_adp_c',:])
+blocked = []
+blocked_rxns = []
+for i in range(fva_res.values.shape[0]):
+    if fva_res.values[i, 0] >= -fva_tol and fva_res.values[i, 1] <= fva_tol:
+        blocked.append(i)
+        blocked_rxns.append(fva_res.index[i])
+print(blocked_rxns)
+
+#%%
+import efmtool_link.efmtool4cobra as efmtool4cobra
+ecc2c = ecc2.copy()
+subT = efmtool4cobra.compress_model_sympy(ecc2c, remove_rxns=blocked_rxns)
+print(len(ecc2c.metabolites), len(ecc2c.reactions))
+rev_rd = [r.reversibility for r in ecc2c.reactions]
+efmtool4cobra.remove_conservation_relations_sympy(ecc2c)
+reduced = cobra.util.array.create_stoichiometric_matrix(ecc2c, array_type='dok', dtype=numpy.object)
+rd = efmtool4cobra.dokRatMat2lilFloatMat(reduced)
+
+
+# %%
