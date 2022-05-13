3cd64291a631f9ba8d27e8c328445dbab1ef5abc
Philipp <VonAlphaBisZulu@users.noreply.github.com>
2022-05-13 17:47:17 +0200

    python script that changes version number
    call:
    python ./.github/update_version.py setup.py 1.0.3
    python ./.github/update_version.py ./conda-recipe/meta.yaml 1.0.3

---
+++ .github/update_version.py
@@ -0,0 +1,11 @@
+import re
+import sys
+file_i = sys.argv[1]
+version = sys.argv[2]
+
+with open (file_i, 'r' ) as f:
+    content = f.read()
+    content_new = re.sub(r"(?<=version\=').*?(?=')", str(version), content, flags = re.M)
+    content_new = re.sub(r"(?<=version \= ').*?(?=')", str(version), content, flags = re.M)
+with open (file_i, 'w' ) as f:
+    f.write(content_new)
