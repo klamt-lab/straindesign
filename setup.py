b13e163da708474afb21ec84997caeb67a87c062
VonAlphaBisZulu <schneiderp@mpi-magdeburg.mpg.de>
2022-05-13 17:23:11 +0000

    Updated version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 
 setup(name='straindesign',
-    version='0.35',
+    version='0.36',
     url='https://github.com/klamt-lab/straindesign.git',
     description="Computational strain design package for the COBRApy framework",
     long_description="Computational strain design package for the COBRApy framework, offering standard and advanced tools for the analysis and redesign of biological networks",
