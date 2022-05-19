504f0ecbce6e56fa4b9459d322b928a7efb71ac6
Philipp <philipp-.-schneider@hotmail.de>
2022-05-19 13:46:25 +0200

    added API doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,13 @@ release = '1.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser']
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'myst_parser']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
