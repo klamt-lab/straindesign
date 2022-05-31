3e29ea7007c4109a892c019e3d26fc08324582bd
Philipp Schneider <schneiderp@mpi-magdeburg.mpg.de>
2022-05-31 18:54:20 +0200

    Update test_02_lp_optimization.py

--- tests/test_02_lp_optimization.py
+++ tests/test_02_lp_optimization.py
@@ -81,35 +81,3 @@ def test_yield_opt_infeasible(curr_solver,model_small_example):
                     solver=curr_solver)
     assert(sol.status == sd.INFEASIBLE)
     
-    
-def load_solvers(model_small_example):
-    """Test solver choice."""
-    # with no solver specified
-    solver1 = sd.select_solver()
-    assert(solver1 in [CPLEX,GUROBI,GLPK,SCIP])
-    
-    # with solver specified
-    solver2 = sd.select_solver('gurobi')
-    assert(solver2 == GUROBI)
-    
-    # with model-specified solver
-    model_small_example.solver = 'glpk'
-    solver3 = sd.select_solver(None, model_small_example)
-    assert(solver3 == GLPK)
-    
-    # with cobrapy-specified solver
-    conf = Configuration()
-    conf.solver = 'cplex'
-    solver4 = sd.select_solver()
-    assert(solver4 == CPLEX)
-    
-    # with solver in model that overwrites the global specification
-    model_small_example.solver = 'scip'
-    solver3 = sd.select_solver(None, model_small_example)
-    assert(solver3 == SCIP)
-    
-    # load solvers
-    from straindesign.cplex_interface import Cplex_MILP_LP
-    from straindesign.gurobi_interface import Gurobi_MILP_LP
-    from straindesign.glpk_interface import GLPK_MILP_LP
-    from straindesign.scip_interface import SCIP_MILP, SCIP_LP
