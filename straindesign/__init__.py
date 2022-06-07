91dfae00407a59fec14053abf5d542c83aa4f983
Philipp <philipp-.-schneider@hotmail.de>
2022-06-07 09:27:25 +0200

    continued documentation

--- straindesign/__init__.py
+++ straindesign/__init__.py
@@ -1,16 +1,16 @@
 from importlib.util import find_spec as module_exists
-
-avail_solvers = []
-if module_exists("swiglpk"):
-    avail_solvers += ["glpk"]
-if module_exists("cplex"):
-    avail_solvers += ["cplex"]
-if module_exists("gurobipy"):
-    avail_solvers += ["gurobi"]
-if module_exists("pyscipopt"):
-    avail_solvers += ["scip"]
-
 from .names import *
+
+avail_solvers = set()
+if module_exists("swiglpk"):
+    avail_solvers.add(GLPK)
+if module_exists("cplex"):
+    avail_solvers.add(CPLEX)
+if module_exists("gurobipy"):
+    avail_solvers.add(GUROBI)
+if module_exists("pyscipopt"):
+    avail_solvers.add(SCIP)
+
 from .solver_interface import *
 from .indicatorConstraints import *
 from .pool import *
