da1b32e30c188bfda642382b5955afa3b15bc7a9
Philipp <VonAlphaBisZulu@users.noreply.github.com>
2026-03-05 16:26:35 -0500

    updated test

--- tests/test_06_multiprocessing.py
+++ tests/test_06_multiprocessing.py
@@ -13,5 +13,8 @@ def test_mcs_larger_model(curr_solver, model_core):
     # modules += [sd.SDModule(model_core, SUPPRESS, constraints=["EX_etoh_e <= 0.5"])]
     modules += [sd.SDModule(model_core, PROTECT, constraints=["BIOMASS_Ec_iML1515_core_75p37M >= 0.1"])]
     sd_setup = {MODULES: modules, MAX_COST: 4, MAX_SOLUTIONS: 0, SOLUTION_APPROACH: ANY, SOLVER: curr_solver, 'compress': False}
+    if curr_solver == GUROBI:
+        logging.info("Enabling compression for Gurobi (uncompressed model exceeds community license limit).")
+        sd_setup['compress'] = True
     solution = sd.compute_strain_designs(model_core, sd_setup=sd_setup)
     assert (len(solution.get_reaction_sd()) == 0)
