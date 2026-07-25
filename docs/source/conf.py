c64c61ac945538c330a3810c327127e6da92fdff
VonAlphaBisZulu <schneiderp@mpi-magdeburg.mpg.de>
2026-07-25 23:09:56 +0000

    updated version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ copyright = "2023, Philipp Schneider"
 author = "Philipp Schneider"
 
 # The full version, including alpha/beta/rc tags
-release = "1.18"
+release = "1.19"
 
 # -- General configuration ---------------------------------------------------
 
